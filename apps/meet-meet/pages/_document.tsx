083f8c9b0e032af605bbe481ee790aeb5d1856a6
SeoyeonKim-gec <seoyeon@gentleenergycorp.com>
2022-08-12 14:01:15 +0900

    :sparkles: PWA 세팅 완료

--- apps/meet-meet/pages/_document.tsx
+++ apps/meet-meet/pages/_document.tsx
@@ -5,11 +5,24 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <body>
-            <Main />
-            <NextScript />
-          </body>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          />
+          <meta name="description" content="Description" />
+          <meta name="keywords" content="Keywords" />
+          <title>MeetMeet</title>
+          <link rel="shortcut icon" href="/favicon_Factoroid_pov.ico" />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icon-192x192.png"></link>
+          <meta name="theme-color" content="#fff" />
         </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
       </Html>
     );
   }
