b1800297c86a84e50be43be3fe7a1cfa4ef0d139
leedaeho1188 <leedaeho1188@gmail.com>
2022-08-24 14:50:23 +0900

    :lipstick: Cell Component 개발 완료

---
+++ packages/ui/src/components/elements/Cells/Cell.tsx
@@ -0,0 +1,45 @@
+import classNames from "classnames";
+import classes from './Cell.module.scss'
+
+interface Props {
+  state: 'default' | 'hover' | 'focused' | 'disable' | 'focusedStartEnd';
+  label: string | number;
+  labelColor?: 'default' | 'error' | 'tertiary';
+  isHover?: boolean;
+  onClick?: () => void;
+  onMouseOver?: () => void;
+  onMouseLeave?: () => void;
+  style?: any; 
+}
+
+
+
+export const Cell = ({
+  state,
+  label,
+  style,
+  labelColor = 'default',
+  onClick,
+  onMouseLeave,
+  onMouseOver,
+  isHover = false,
+}:Props) => {
+
+  return (
+    <div 
+      className={classNames(classes.cellContainer, classes[state], classes[labelColor], isHover && classes.isHover)}
+      style={{...style}}
+    >
+      <div className={classes.cellText}>
+        {label}
+      </div>
+      <div 
+        className={classes.stateLayer}
+        onClick={onClick}
+        onMouseOver={onMouseOver}
+        onMouseLeave={onMouseLeave} 
+      > 
+      </div>
+    </div>
+  )
+}
