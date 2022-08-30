26513f45136d97da0f26c0ac30505e5221192d68
SeoyeonKim-gec <seoyeon@gentleenergycorp.com>
2022-08-30 16:31:22 +0900

    :sparkles: / -> /home 으로 라우팅

--- apps/meet-meet/pages/home/index.tsx
+++ apps/meet-meet/pages/home/index.tsx
@@ -1,10 +1,12 @@
-import { Button, CellGroup, ScrollDrag } from "@components/ui";
-import { useEffect, useState } from "react";
-import { CalendarLayout, Cell } from "@components/ui";
+import { useState } from "react";
+import type { NextPage } from "next";
+import { Reservation } from "components";
+import { CalendarLayout, Button } from "@components/ui";
 import { formatDate } from "ui/src/utils";
+import { Text } from "ui/src/pages";
 
-const Home = () => {
-  const [btnState, setBtnState] = useState<boolean>(true);
+const Home: NextPage = () => {
+  const [btnState, setBtnState] = useState<boolean>(false);
   const [date, setDate] = useState<Date>(
     new Date(
       new Date().getFullYear(),
@@ -12,61 +14,50 @@ const Home = () => {
       new Date().getDate() - 1
     )
   );
-  const [defaultIndex, setDefaultIndex] = useState({ start: null, end: null });
 
   return (
-    <div>
-      <Button
-        configuration="outlined"
-        size="large"
-        label={formatDate(date)}
-        onClick={() => setBtnState(true)}
-      />
-      {btnState && (
-        <CalendarLayout
-          setCalendar={setBtnState}
-          onClickSubmitBtn={(startDate: any) => {
-            setDate(startDate);
-            setBtnState(false);
-          }}
-          date={date}
-          start={date}
-          end={date}
-          type="single"
-          timeType="futureCurrent"
-        />
-      )}
-
-      <div style={{ width: "500px" }}>
-        <CellGroup disableIndex={[3]} defaultIndex={defaultIndex}>
-          <Cell label="18:00" />
-          <Cell label="18:00" />
-          <Cell label="18:00" />
-          <Cell
-            label="18:00"
-            style={{
-              width: "300px",
-              backgroundColor: "var(--color-primary)",
-              color: "var(--color-onSurface)",
-            }}
+    <div
+      style={{
+        position: "absolute",
+        top: "50px",
+        right: "40px",
+        bottom: "24px",
+        left: "24px",
+        display: "grid",
+      }}
+    >
+      <div>
+        <Text
+          type="headline-large"
+          color="primary"
+          style={{ fontWeight: "bold" }}
+        >
+          젠틀에너지 회의실 예약 현황
+        </Text>
+        <div>
+          <Button
+            configuration="outlined"
+            size="large"
+            label={formatDate(date)}
+            onClick={() => setBtnState(true)}
           />
-          <Cell label="18:00" />
-          <Cell label="18:00" />
-          <Cell label="18:00" />
-          <Cell label="18:00" />
-          <Cell label="18:00" />
-          <Cell label="18:00" />
-          <Cell label="18:00" />
-          <Cell label="18:00" />
-          <Cell label="18:00" />
-        </CellGroup>
+        </div>
+        {btnState && (
+          <CalendarLayout
+            setCalendar={setBtnState}
+            onClickSubmitBtn={(startDate: any) => {
+              console.log(startDate);
+              setDate(startDate);
+              setBtnState(false);
+            }}
+            date={date}
+            start={date}
+            end={date}
+            type="single"
+          />
+        )}
       </div>
-      <Button
-        label="버튼"
-        onClick={() => {
-          setDefaultIndex({ start: null, end: null });
-        }}
-      />
+      <Reservation startDate={date} />
     </div>
   );
 };
--- apps/meet-meet/pages/index.tsx
+++ apps/meet-meet/pages/index.tsx
@@ -1,67 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import type { NextPage } from "next";
-import { Reservation, Layout } from "components";
-import { CalendarLayout, Button } from "@components/ui";
-import { formatDate } from "ui/src/utils";
-import { Text } from "ui/src/pages";
+import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
-  const [btnState, setBtnState] = useState<boolean>(false);
-  const [date, setDate] = useState<Date>(
-    new Date(
-      new Date().getFullYear(),
-      new Date().getMonth(),
-      new Date().getDate() - 1
-    )
-  );
+  const router = useRouter();
+  useEffect(() => {
+    router.replace("/home");
+  }, []);
 
-  return (
-    <Layout>
-      <div
-        style={{
-          position: "absolute",
-          top: "50px",
-          right: "40px",
-          bottom: "24px",
-          left: "24px",
-          display: "grid",
-        }}
-      >
-        <div>
-          <Text
-            type="headline-large"
-            color="primary"
-            style={{ fontWeight: "bold" }}
-          >
-            젠틀에너지 회의실 예약 현황
-          </Text>
-          <div>
-            <Button
-              configuration="outlined"
-              size="large"
-              label={formatDate(date)}
-              onClick={() => setBtnState(true)}
-            />
-          </div>
-          {btnState && (
-            <CalendarLayout
-              setCalendar={setBtnState}
-              onClickSubmitBtn={(startDate: any) => {
-                console.log(startDate);
-                setDate(startDate);
-                setBtnState(false);
-              }}
-              date={date}
-              start={date}
-              end={date}
-              type="single"
-            />
-          )}
-        </div>
-        <Reservation startDate={date} />
-      </div>
-    </Layout>
-  );
+  return <></>;
 };
 
 export default Home;
