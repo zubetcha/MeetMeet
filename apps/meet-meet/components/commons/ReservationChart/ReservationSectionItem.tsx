72211f5bc4819edbd0cc3afb6f1a124acc81aea3
SeoyeonKim-gec <seoyeon@gentleenergycorp.com>
2022-09-13 17:49:56 +0900

    :recycle: ReservationSection, ReservationSectionItem 분리

---
+++ apps/meet-meet/components/commons/ReservationChart/ReservationSectionItem.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import classes from "./reservation.module.scss";
+import { Text } from "ui/src/pages";
+
+interface Props {
+  date: string;
+  meetingRoomList: string[];
+}
+
+/**
+ *
+ * @param date (string) 날짜 ex) 08.23(화)
+ * @param meetingRoomList (string) 미팅룸 리스트
+ * @returns
+ */
+export default function ReservationSectionItem({
+  date,
+  meetingRoomList,
+}: Props) {
+  return (
+    <div className={classes.roomList}>
+      <div className={classes.infoContainer}>
+        <div className={classes.dateContainer}>
+          <Text color="on-primary" style={{ weight: "bold" }}>
+            {date}
+          </Text>
+        </div>
+        <div className={classes.roomContainer}>
+          {meetingRoomList.map((room: string, index) => (
+            <div className={classes.slotRoom} key={`room-${index}`}>
+              <Text type="body-small">{room}</Text>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
