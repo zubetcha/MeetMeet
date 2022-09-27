e2611df0b98b4a834806848989a7b71ae4b22846
이대호 <leedaeho1188@gentleenergycorp.com>
2022-09-27 11:14:51 +0900

    :lipstick: 회의실 예약 버튼 ui mobile-verti 수정

--- apps/meet-meet/pages/_app.tsx
+++ apps/meet-meet/pages/_app.tsx
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
+import { useCallback, useEffect, useMemo } from "react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { debounce } from "lodash";
 
 import { ApolloProvider } from "@apollo/client";
 import { ReactQueryDevtools } from "react-query/devtools";
@@ -36,14 +37,23 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, [])
 
   useEffect(() => {
+    debounceInnerHeight();
+    window.addEventListener('resize', debounceInnerHeight);
+
+    return () => {
+      window.removeEventListener('resize', debounceInnerHeight);
+    }
+  }, [])
+
+  const setVhProperty = () => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
 
-    window.addEventListener('resize', () => {
-      let vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
-    });
-  }, [])
+  const debounceInnerHeight = useMemo(
+    () => debounce(setVhProperty, 300)
+  , []);
+
 
   return (
     <>
