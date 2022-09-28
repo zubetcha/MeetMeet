cedc855765dd55d76a9e49e774d45b212471e12e
Juhye-gec <jhjeong@gentleenergycorp.com>
2022-09-28 16:14:43 +0900

    :wrench: 구글 oauth url 수정

--- apps/meet-meet/constants/auth.tsx
+++ apps/meet-meet/constants/auth.tsx
@@ -1,5 +1,4 @@
-import { isProduction } from "./common";
-import { API_BASE_URL } from "./common";
+import { isProduction, SERVER_URL, SERVER_LOCAL_URL } from "./common";
 
 export const ACCESS_TOKEN = 'accessToken';
 export const REFRESH_TOKEN = 'refreshToken';
@@ -7,5 +6,6 @@ export const REFRESH_TOKEN = 'refreshToken';
 export const LOCAL_OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth/redirect';
 export const PROD_OAUTH2_REDIRECT_URI = 'https://www.meetmeet.me/oauth/redirect';
 export const OAUTH2_REDIRECT_URI = isProduction ? PROD_OAUTH2_REDIRECT_URI : LOCAL_OAUTH2_REDIRECT_URI;
+export const API_BASE_URL = isProduction ? SERVER_URL : SERVER_LOCAL_URL;
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorization/google?redirect_uri' + OAUTH2_REDIRECT_URI;
