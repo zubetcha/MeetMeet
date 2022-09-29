6c94a1216a9890af53395719d00d2b1d246b8293
Juhye-gec <jhjeong@gentleenergycorp.com>
2022-09-29 11:12:41 +0900

    :lipstick: 회의실 이미지 프리뷰 삭제 버튼 마우스오버 시에만 보이도록 수정

--- apps/meet-meet/components/management/ImagePreview.tsx
+++ apps/meet-meet/components/management/ImagePreview.tsx
@@ -1,9 +1,10 @@
-import { Dispatch, SetStateAction } from 'react';
+import { useState, Dispatch, SetStateAction } from 'react';
 import classes from "./management.module.scss";
 import Image from "next/image";
 import { IconButton } from 'ui/src/pages';
 
 export const ImagePreview = ({ url, setImages }: Props) => {
+  const [isHover, setIsHover] = useState(false);
   const onClickCancel = () => {
     if (setImages) {
       setImages(prev => prev.filter(image => image.url !== url));
@@ -11,11 +12,15 @@ export const ImagePreview = ({ url, setImages }: Props) => {
     }
   }
   return (
-      <div className={classes["preview-container"]}>
+      <div
+        className={classes["preview-container"]}
+        onMouseOver={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+      >
         <div className={classes["preview-wrapper"]}>
           <Image src={url} className={classes.preview} layout="fill" alt="preview"/>
         </div>
-        {setImages && (
+        {setImages && isHover && (
           <div className={classes["button-wrapper"]}>
             <IconButton icon="close" configuration="filled" size="small" state="default" onClick={onClickCancel} />
           </div>
