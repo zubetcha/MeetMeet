c372c4bae263904ded98b83b9cbaf157bf3af763
Juhye-gec <jhjeong@gentleenergycorp.com>
2022-10-05 09:19:01 +0900

    :heavy_plus_sign: compressorjs 설치

--- apps/meet-meet/pages/_document.tsx
+++ apps/meet-meet/pages/_document.tsx
@@ -6,7 +6,6 @@ export default function Document() {
     <Html>
       <Head>
         <meta charSet="utf-8"></meta>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"></meta>
         <meta name="mobile-web-app-capable" content="yes"></meta>
         <meta name="apple-mobile-web-app-title" content="MeetMeet"></meta>
         <meta name="apple-mobile-web-app-capable" content="yes"></meta>
