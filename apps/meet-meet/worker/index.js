f9801ab89d646a1284cf7c699b26dbf16fd889f2
zubetcha <zuhye5@gmail.com>
2022-11-01 23:58:38 +0900

    :wrench: firebase-messaging-sw app config update

--- apps/meet-meet/worker/index.js
+++ apps/meet-meet/worker/index.js
@@ -1,23 +1,23 @@
-"use strict";
+'use strict';
 
 // To disable all workbox logging during development, you can set self.__WB_DISABLE_DEV_LOGS to true
 // https://developers.google.com/web/tools/workbox/guides/configure-workbox#disable_logging
-self.__WB_DISABLE_DEV_LOGS = true
+self.__WB_DISABLE_DEV_LOGS = true;
 
-importScripts("https://www.gstatic.com/firebasejs/9.5.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.5.0/firebase-messaging-compat.js");
+importScripts('https://www.gstatic.com/firebasejs/9.5.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.5.0/firebase-messaging-compat.js');
 
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyB5MsSaVHen868J6lRWD1R4bQX0jH5K7qE",
-  authDomain: "meetmeet-a49ad.firebaseapp.com",
-  projectId: "meetmeet-a49ad",
-  storageBucket: "meetmeet-a49ad.appspot.com",
-  messagingSenderId: "831729942382",
-  appId: "1:831729942382:web:4aa84d00b6d73b28ad4b7f",
-  measurementId: "G-41K21FTLLR"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background messages.
@@ -27,7 +27,10 @@ if (isSupported) {
 
   messaging.onBackgroundMessage((payload) => {
     console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    const { notification: { title, body }, data: { reservation } } = payload;
+    const {
+      notification: { title, body },
+      data: { reservation },
+    } = payload;
     const reservationId = parseInt(reservation);
     self.registration.showNotification(title, { body });
   });
